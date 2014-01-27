d8a48634e776b6766f9b055426e519dda983de34
Eric Schneider <franz.eric.schneider@gmail.com>
2014-01-27 17:30:44 -0500

    Added a kd term to the controller and made it able to sweep parameters to find the best PID values

--- hw01/ch01.py
+++ hw01/ch01.py
@@ -45,14 +45,18 @@ class Buffer:
         return self.queued
 
 class Controller:
-    def __init__( self, kp, ki ):
+    def __init__( self, kp, ki, kd ):
         """Initializes the controller.
 
         kp: proportional gain
         ki: integral gain
+        kp: derivative gain
         """
-        self.kp, self.ki = kp, ki
+        self.kp, self.ki, self.kd = kp, ki, kd
         self.i = 0       # Cumulative error ("integral")
+
+        self.memory = 0  # Previous value, for computing RoC on the error
+        self.d = 0;      # RoC on error ("derivative")
 
     def work( self, e ):
         """Computes the number of jobs to be added to the ready queue.
@@ -62,8 +66,9 @@ class Controller:
         returns: float number of jobs
         """
         self.i += e
+        self.d = self.memory-e;
 
-        return self.kp*e + self.ki*self.i
+        return self.kp*e + self.ki*self.i + self.kd*self.d;
 
 # ============================================================
 
@@ -77,6 +82,10 @@ def closed_loop( c, p, tm=5000 ):
     returns: tuple of sequences (times, targets, errors)
     """
     def setpoint( t ):
+        ## Sloped line
+        # return t/10
+        ## OR
+        ## Step function
         if t < 100: return 0
         if t < 300: return 50
         return 10
@@ -96,13 +105,61 @@ def closed_loop( c, p, tm=5000 ):
 
 # ============================================================
 
-c = Controller( 1.25, 0.01 )
+def find_best_performers(RMS, p, i, d):
+    """Finds the 5 best RMS values in a simulation and records the PID values used
+
+    RMS: Root Mean Square error of a recently performed simulation
+    p, i, d: The PID constants that produced the given RMS
+
+    No return, alters the 'best_performers' dictionary
+    """
+
+    max_RMS = max(best_performers.keys())
+    if len(best_performers) < 5:
+        RMS = int(RMS * 100) / 100.0
+        best_performers[RMS] = [p, i, d]
+    elif RMS < max_RMS:
+        del best_performers[max_RMS]
+        RMS = int(RMS * 100) / 100.0
+        best_performers[RMS] = [p, i, d]
+
+# ============================================================
+
+# set up the parameter sweeps to find the best PID values
+best_performers = {100: [0, 0, 0]}
+p_vals = []; i_vals = []; d_vals = [];
+for x in range(0, 15, 1):
+    p_vals.append(x/10.0)
+    i_vals.append(x/100.0)
+    # i_vals.append(0) ## Uncomment this to see what a zeroed integral term does
+    d_vals.append(x/100.0)
+print p_vals
+
+for i in range(len(p_vals)):
+    for j in range(len(i_vals)):
+        for k in range(len(d_vals)):
+            c = Controller( p_vals[i], i_vals[j], d_vals[k] )
+            p = Buffer( 50, 10 )
+
+            # run the simulation
+            ts, rs, es, us, ys = closed_loop( c, p, 1000 )
+
+            # find RMS and compare it to previous runs
+            RMS = numpy.sqrt(numpy.mean(numpy.array(es)**2));
+            find_best_performers(RMS, p_vals[i], i_vals[j], d_vals[k])
+
+print best_performers
+
+pid = best_performers[min(best_performers.keys())]
+c = Controller( pid[0], pid[1], pid[2] )
 p = Buffer( 50, 10 )
 
 # run the simulation
 ts, rs, es, us, ys = closed_loop( c, p, 1000 )
 
-print 'RMS error', numpy.sqrt(numpy.mean(numpy.array(es)**2))
+# find RMS and compare it to previous runs
+RMS = numpy.sqrt(numpy.mean(numpy.array(es)**2));
+print 'RMS error', RMS
 
 # generate the smoothed curve using a rolling mean
 # (I think the curves in the book use loess)
@@ -113,6 +170,3 @@ pyplot.plot(ts, rs, color='green', label='target')
 pyplot.plot(ts, ys, color='red', label='queue length')
 pyplot.plot(ts, ys_smooth, color='blue', label='trend')
 pyplot.show()
-
-
-
