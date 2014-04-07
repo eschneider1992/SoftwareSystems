d1ab2afdc821df0f586b8ea00f8681f0b26a5725
Eric Schneider <franz.eric.schneider@gmail.com>
2014-04-07 01:21:50 -0400

    Commented the Glib code

--- hw05/glib-2.36.3/wordfreq.c
+++ hw05/glib-2.36.3/wordfreq.c
@@ -1,5 +1,9 @@
-// Drawn from http://cboard.cprogramming.com/c-programming/40527-reading-file-word-word.html
-// Drawn from http://stackoverflow.com/questions/18109458/read-from-a-text-file-and-parse-lines-into-words-in-c
+/*
+* Eric Schneider
+* 04/07/2014
+* Drawn from http://cboard.cprogramming.com/c-programming/40527-reading-file-word-word.html
+* Drawn from http://stackoverflow.com/questions/18109458/read-from-a-text-file-and-parse-lines-into-words-in-c
+*/
 
 #include <glib.h>
 #include <glib/gprintf.h>
@@ -8,7 +12,6 @@
 #include <ctype.h>
 
 #define BUF_LEN 30
-
 
 void extract_words(FILE *input_file, GHashTable *htable);
 void add_word_to_table(GHashTable *htable, GString *strbuf);
@@ -22,12 +25,19 @@ GList *allKeys;
 GList *allValues;
 guint numWords;
 
+
+/*
+* wordfreq.c opens a constant file, pulls the words out, writes unique words to
+* a glib hashtable, and increments the frequency of non-unique words.
+*/
 int main (int argc, char **argv)
 {
 	char *input = "test.txt";
 	FILE *input_file;
 
+	// opens the file
 	input_file = fopen(input, "r");
+
 	if (input_file == 0)
 	{
 		fprintf(stderr, "Cannot open input file\n");
@@ -37,15 +47,18 @@ int main (int argc, char **argv)
 		htable = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
 		key = g_malloc(sizeof(gchar *));
 		val = g_malloc(sizeof(guint *));
+		// pulls words out of file
 		extract_words(input_file, htable);
 	}
 
+	// prints the words and their frequencies
 	numWords = g_hash_table_size(htable);
 	g_printf("\nThere are %d unique words in the file: \n", numWords);
 	allKeys = g_new(GList, 1);
 	allKeys = g_hash_table_get_keys(htable);
 	g_list_foreach(allKeys, print_key_val, NULL);
 
+	// tries to free all the allocated memory
 	fclose(input_file);
 	g_hash_table_destroy(htable);
 	g_free(val);
@@ -54,6 +67,12 @@ int main (int argc, char **argv)
 	return 0;
 }
 
+/*
+* Takes an input file and a Glib hashtable, then calls add_word_to_table on each word
+* in the file, separated by whitespace. All characters are set to lowercase here.
+*
+* TODO: add a filter for punctuation
+*/
 void extract_words(FILE *input_file, GHashTable *htable) {
 	char buf[BUF_LEN];
 	GString *strbuf = g_string_new(NULL);
@@ -66,6 +85,12 @@ void extract_words(FILE *input_file, GHashTable *htable) {
 	g_free(strbuf);
 }
 
+/*
+* Takes in a Glib hashtable and a GString variable. Checks whether the string is already
+* contained in the hashtable as a key. If so, the corresponding value is incremented. If
+* not, a value of 1 entered alongside the string. g_strdup ensures that each key points to
+* a unique string
+*/
 void add_word_to_table(GHashTable *htable, GString *strbuf) {
 	key = g_strdup(strbuf->str);
 	val = (guint*) g_hash_table_lookup(htable, key);
@@ -81,6 +106,10 @@ void add_word_to_table(GHashTable *htable, GString *strbuf) {
 	}
 }
 
+/*
+* Takes a Glib hashtable key and then prints the key and the value
+* data2 is unused 
+*/
 void print_key_val (gpointer data1, gpointer data2){
     key = (gchar *) data1;
 	val = (guint*) g_hash_table_lookup(htable, key);
